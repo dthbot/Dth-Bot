864d330fdbfde858d14f72dc8e4657943946d16d
dthbot <blebledarius@gmail.com>
2025-12-16 13:11:39 +0100

    Update fun-pompino.js

--- plugins/fun-pompino.js
+++ plugins/fun-pompino.js
@@ -1,17 +1,26 @@
-let handler = async (m, { conn, text, participants }) => {
+let handler = async (m, { conn }) => {
     let utentiMenzionati = m.mentionedJid;
     
     if (!utentiMenzionati.length) {
-        return m.reply("😏 *Devi menzionare qualcuno per farti fare un pompino!* \nEsempio: _.pompino @utente_");
+        return m.reply("😏 *Devi menzionare qualcuno per farti fare un pompino!*\nEsempio: _.pompino @utente_");
     }
 
     let utenteScelto = utentiMenzionati[0];
+    let mittente = m.sender;
 
-    let messaggio = `💋 *${await conn.getName(m.sender)} 𝐬𝐢 è 𝐟𝐚𝐭𝐭𝐨 𝐟𝐚𝐫𝐞 𝐮𝐧 𝐩𝐨𝐦𝐩𝐢𝐧𝐨 𝐝𝐚 @${utenteScelto.split("@")[0]}!* 🔥\n\n😏 𝐒𝐩𝐞𝐫𝐢𝐚𝐦𝐨 𝐜𝐡𝐞 𝐬𝐢𝐚 𝐬𝐭𝐚𝐭𝐨 𝐝𝐢 𝐭𝐮𝐨 𝐠𝐫𝐚𝐝𝐢𝐦𝐞𝐧𝐭𝐨...`;
+    let messaggio = `💋 *@${mittente.split("@")[0]} 𝐬𝐢 è 𝐟𝐚𝐭𝐭𝐨 𝐟𝐚𝐫𝐞 𝐮𝐧 𝐩𝐨𝐦𝐩𝐢𝐧𝐨 𝐝𝐚 @${utenteScelto.split("@")[0]}!* 🔥
 
-    await conn.sendMessage(m.chat, { text: messaggio, mentions: [utenteScelto] }, { quoted: m });
+😏 𝐒𝐩𝐞𝐫𝐢𝐚𝐦𝐨 𝐜𝐡𝐞 𝐬𝐢𝐚 𝐬𝐭𝐚𝐭𝐨 𝐝𝐢 𝐭𝐮𝐨 𝐠𝐫𝐚𝐝𝐢𝐦𝐞𝐧𝐭𝐨...`;
+
+    await conn.sendMessage(
+        m.chat,
+        { 
+            text: messaggio, 
+            mentions: [mittente, utenteScelto] 
+        },
+        { quoted: m }
+    );
 };
 
-// Definizione del comando per Gab
 handler.command = ["pompino"];
 export default handler;
