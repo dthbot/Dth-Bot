283fbb8938c85d57b63d377f7a0269c0b8b50558
dthbot <blebledarius@gmail.com>
2025-12-17 13:59:41 +0100

    Update adm-vampexe.js

--- plugins/adm-vampexe.js
+++ plugins/adm-vampexe.js
@@ -1,23 +1,23 @@
-const fs = require('fs');
+import fs from 'fs'
 
-module.exports = async (client, message) => {
-    // Trasforma il testo in minuscolo per evitare errori di battitura
-    const body = message.body.toLowerCase();
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+    const imagePath = './media/vamp1.jpeg'
+    const caption = "𝐕𝐚𝐦𝐩𝐞xe è 𝐥𝐚 𝐦𝐨𝐠𝐥𝐢𝐞 𝐝𝐢 𝕯𝖊ⱥ𝖉𝖑𝐲, 𝐭𝐮𝐭𝐭𝐢 𝐬𝐚𝐧𝐧𝐨 𝐜𝐡𝐞 𝐧𝐨𝐧 𝐥𝐚 𝐝𝐞𝐯𝐨𝐧𝐨 𝐭𝐨𝐜𝐜𝐚𝐫𝐞 𝐬𝐞𝐧𝐧ò 𝐬𝐨𝐧𝐨 𝐠𝐮𝐚𝐢 🖤"
 
-    if (body === 'vampexe') {
-        const imagePath = './media/vamp1.jpeg';
-        const caption = "𝐕𝐚𝐦𝐩𝐞𝐱𝐞 è 𝐥𝐚 𝐦𝐨𝐠𝐥𝐢𝐞 𝐝𝐢 𝕯𝖊ⱥ𝖉𝖑𝐲, 𝐭𝐮𝐭𝐭𝐢 𝐬𝐚𝐧𝐧𝐨 𝐜𝐡𝐞 𝐧𝐨𝐧 𝐥𝐚 𝐝𝐞𝐯𝐨𝐧𝐨 𝐭𝐨𝐜𝐜𝐚𝐫𝐞 𝐬𝐞𝐧𝐧ò 𝐬𝐨𝐧𝐨 𝐠𝐮𝐚𝐢 🖤";
-
-        // Verifica se il file esiste prima di inviarlo
-        if (fs.existsSync(imagePath)) {
-            await client.sendMessage(message.from, {
-                image: { url: imagePath },
-                caption: caption,
-                // L'opzione viewOnce: true imposta la foto in modalità "x1"
-                viewOnce: true 
-            });
-        } else {
-            console.error("Errore: Il file media/vamp1.jpeg non è stato trovato.");
-        }
+    if (fs.existsSync(imagePath)) {
+        await conn.sendMessage(m.chat, {
+            image: fs.readFileSync(imagePath),
+            caption: caption,
+            viewOnce: true
+        }, { quoted: m })
+    } else {
+        m.reply("Errore: La foto non è stata trovata nella cartella media/vamp1.jpeg")
     }
-};
+}
+
+// Qui impostiamo il comando che attiva il bot
+handler.customPrefix = /vampexe/i
+handler.command = new RegExp
+handler.tags = ['premium']
+
+export default handler
