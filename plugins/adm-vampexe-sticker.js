2d16e7c3024a309979d2df98ce87632053cf2cd4
dthbot <blebledarius@gmail.com>
2025-12-17 14:30:49 +0100

    Update adm-vampexe-sticker.js

--- plugins/adm-vampexe-sticker.js
+++ plugins/adm-vampexe-sticker.js
@@ -1,17 +1,14 @@
 import fs from 'fs'
 
 let handler = async (m, { conn }) => {
-    const stickerPath = './media/vampexe.webp'
-
-    if (!fs.existsSync(stickerPath)) {
-        return m.reply("⚠️ Il file media/vampexe.webp non esiste!")
-    }
-
-    // Usiamo sendFile che gestisce automaticamente la conversione e i metadati
-    await conn.sendFile(m.chat, stickerPath, 'sticker.webp', '', m, { asSticker: true })
+  const stickerPath = './media/vampexe.webp'
+  if (!fs.existsSync(stickerPath)) {
+    return m.reply("⚠️ Il file media/vampexe.webp non esiste!")
+  }
+  // Usiamo sendSticker che gestisce automaticamente la conversione e i metadati
+  await conn.sendSticker(m.chat, stickerPath, m, { pack: 'Vampexe', author: '𝕯𝖊ⱥ𝖉𝖑𝐲' })
 }
 
-handler.customPrefix = /vamp/i 
+handler.customPrefix = /vamp/i
 handler.command = new RegExp
-
 export default handler
