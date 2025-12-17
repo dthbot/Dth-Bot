b40bca0bbe6859fdbfcc5fc9c99c346e208d8aba
dthbot <blebledarius@gmail.com>
2025-12-17 14:23:23 +0100

    Update adm-vampexe-sticker.js

--- plugins/adm-vampexe-sticker.js
+++ plugins/adm-vampexe-sticker.js
@@ -4,15 +4,20 @@ let handler = async (m, { conn }) => {
     const stickerPath = './media/vampexe.webp'
 
     if (fs.existsSync(stickerPath)) {
+        // Leggiamo il file
+        let stiker = fs.readFileSync(stickerPath)
+        
+        // Inviamo come sticker usando il metodo corretto
         await conn.sendMessage(m.chat, { 
-            sticker: fs.readFileSync(stickerPath) 
+            sticker: stiker 
         }, { quoted: m })
+
     } else {
-        m.reply("Errore: Lo sticker non è stato trovato in media/vampexe.webp")
+        // Se non lo trova, ti avvisa nel gruppo o in chat
+        m.reply("⚠️ Errore: File 'media/vampexe.webp' non trovato. Controlla che il nome sia corretto!")
     }
 }
 
-// Il bot reagisce quando qualcuno scrive "vamp"
 handler.customPrefix = /vamp/i 
 handler.command = new RegExp
 
