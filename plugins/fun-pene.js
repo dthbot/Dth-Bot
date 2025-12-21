186187623c71bc3a4b67659801c8c8c74ceaf018
dthbot <blebledarius@gmail.com>
2025-12-21 16:08:19 +0100

    Update fun-pene.js

--- plugins/fun-pene.js
+++ plugins/fun-pene.js
@@ -1,18 +1,22 @@
 let handler = async (m, { conn, command, text }) => {
+    let user = m.sender.split('@')[0]
+
     let message = `
 *📏 𝐜𝐚𝐥𝐜𝐨𝐥𝐚𝐭𝐨𝐫𝐞 𝐝𝐢 𝐦𝐢𝐬𝐮𝐫𝐚 📏*
 
 ━━━━━━━━━━━━━━━━━━━━━
-🔍 *${text}* 𝐡𝐚 𝐮𝐧𝐚 𝐥𝐮𝐧𝐠𝐡𝐞𝐳𝐳𝐚 𝐬𝐭𝐢𝐦𝐚𝐭𝐚 𝐝𝐢:
+🔍 *@${user}* 𝐡𝐚 𝐮𝐧𝐚 𝐥𝐮𝐧𝐠𝐡𝐞𝐳𝐳𝐚 𝐬𝐭𝐢𝐦𝐚𝐭𝐚 𝐝𝐢:
 👉 *${Math.floor(Math.random() * 101)} 𝐜𝐦*
 ━━━━━━━━━━━━━━━━━━━━━
-`.trim();
+`.trim()
 
-    m.reply(message, null, { mentions: conn.parseMention(message) });
-};
+    m.reply(message, null, {
+        mentions: [m.sender]
+    })
+}
 
-handler.help = ['calcolatore'];
-handler.tags = ['divertimento'];
-handler.command = /^(pene)$/i;
+handler.help = ['calcolatore']
+handler.tags = ['divertimento']
+handler.command = /^(pene)$/i
 
-export default handler;
+export default handler
