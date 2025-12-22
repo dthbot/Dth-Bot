27a945c22d9e74e10ec5e54f7ba4a0944629c147
dthbot <blebledarius@gmail.com>
2025-12-22 18:58:25 +0100

    Update fun-sbirro.js

--- plugins/fun-sbirro.js
+++ plugins/fun-sbirro.js
@@ -1,15 +1,17 @@
-const handler = async (msg, { client }) => {
-  // Genera una percentuale casuale tra 0 e 100
-  const percent = Math.floor(Math.random() * 101);
-  
-  const response = `👮‍♂️ Il tuo livello di sbirro è: *${percent}%* 🚔`;
-  
-  await client.sendMessage(msg.chat, { text: response }, { quoted: msg });
-};
+const handler = async (msg, { conn }) => {
+  const percent = Math.floor(Math.random() * 101)
 
-// Configurazione del comando
-handler.command = ['sbirro', 'quantosbirro'];
-handler.category = 'fun';
-handler.desc = 'Scopri quanto sei uno sbirro 🚓';
+  const response = `👮‍♂️ Il tuo livello di sbirro è: *${percent}%* 🚔`
 
-export default handler;
+  await conn.sendMessage(
+    msg.chat,
+    { text: response },
+    { quoted: msg }
+  )
+}
+
+handler.command = ['sbirro', 'quantosbirro']
+handler.category = 'fun'
+handler.desc = 'Scopri quanto sei uno sbirro 🚓'
+
+export default handler
