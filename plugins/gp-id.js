fcf78fa182e255411fa8c45b45ea4087af15c447
dthbot <blebledarius@gmail.com>
2025-12-23 05:06:25 +0100

    Update gp-id.js

--- plugins/gp-id.js
+++ plugins/gp-id.js
@@ -5,7 +5,7 @@ var handler = async (m, { conn, groupMetadata }) => {
             forwardingScore: 99,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363259442839354@newsletter',
+                newsletterJid: '',
                 serverMessageId: '',
                 newsletterName: 'ChatUnity'
             }
