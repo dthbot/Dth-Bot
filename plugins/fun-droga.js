e0fe0eaa430505108d382627400820448e490379
dthbot <blebledarius@gmail.com>
2025-12-24 03:56:02 +0100

    Update fun-droga.js

--- plugins/fun-droga.js
+++ plugins/fun-droga.js
@@ -1,29 +1,31 @@
 let handler = async (m, { conn }) => {
 
-  // SE PREMUTO UN BOTTONE
-  if (m.message?.buttonsResponseMessage) {
-    let scelta = m.message.buttonsResponseMessage.selectedButtonId
+  // 👉 RISPOSTA AI BOTTONI (FIX)
+  let btnId =
+    m.message?.templateButtonReplyMessage?.selectedId ||
+    m.message?.buttonsResponseMessage?.selectedButtonId
 
+  if (btnId) {
     return conn.sendMessage(m.chat, {
       text:
-`✅ *VENDUTO*
+`✅ VENDUTO
 
-🚬 "${scelta}" *È la tua scelta? Bene ne hai comprato 5 Grammi*
+🚬 "${scelta}" È la tua scelta? Bene ne hai comprato 5 Grammi
 
-🤪 *GODITELI*
+🤪 GODITELI
 
-💪 *SI alle droghe*`
+💪 SI alle droghe`
     }, { quoted: m })
   }
 
-  // COMANDO .droga
+  // 👉 COMANDO .droga
   await conn.sendMessage(m.chat, {
     text: `🤔 *Che droga vuoi prendere?*`,
     buttons: [
-      { buttonId: 'PolvereMagica', buttonText: { displayText: '⚗️ Oppio' }, type: 1 },
-      { buttonId: 'SuccoArcobaleno', buttonText: { displayText: '🍫 Fumo' }, type: 1 },
-      { buttonId: 'NebbiaBlu', buttonText: { displayText: '🌿 Erba' }, type: 1 },
-      { buttonId: 'PillolaXP', buttonText: { displayText: '💨 Cocaina' }, type: 1 }
+      { buttonId: 'Oppio', buttonText: { displayText: '⚗️ Oppio' }, type: 1 },
+      { buttonId: 'Fumo', buttonText: { displayText: '🍫 Fumo' }, type: 1 },
+      { buttonId: 'Erba', buttonText: { displayText: '🌿 Erba' }, type: 1 },
+      { buttonId: 'Cocaina', buttonText: { displayText: '💨 Cocaina' }, type: 1 }
     ],
     headerType: 1
   }, { quoted: m })
