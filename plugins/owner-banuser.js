f3e094eb9e50fa8fbfab274f66ab590006c0ca47
dthbot <blebledarius@gmail.com>
2025-12-25 21:27:56 +0100

    Update owner-banuser.js

--- plugins/owner-banuser.js
+++ plugins/owner-banuser.js
@@ -1,31 +1,36 @@
-async function handler(m, { conn, participants, usedPrefix, command }) {
-  if (!m.mentionedJid[0] && !m.quoted) return;
-  let target;
-  if (m.isGroup) {
-    target = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender;
-  } else {
-    target = m.chat;
-  }
-  
-  const protectedNumbers = ["393924311304@s.whatsapp.net", "393201448716@s.whatsapp.net"];
-  
-  if (protectedNumbers.includes(target)) {
-    return conn.reply(m.chat, "ⓘ 𝐍𝐨𝐧 𝐩𝐮𝐨𝐢 𝐛𝐚𝐧𝐧𝐚𝐫𝐞 𝐢𝐥 𝐜𝐫𝐞𝐚𝐭𝐨𝐫𝐞 𝐞 𝐢𝐥 𝐛𝐨𝐭", m);
-  }
-  
-  global.db.data.users[target].banned = true;
-  const vcardMessage = {
-    key: {
-      participants: "0@s.whatsapp.net",
-      fromMe: false,
-      id: "Halo"
-    },
-    message: {
-      locationMessage: {
-        name: "𝐔𝐭𝐞𝐧𝐭𝐞 bloccato",
-        jpegThumbnail: await (await fetch("https://telegra.ph/file/710185c7e0247662d8ca6.png")).buffer(),
-        vcard: `BEGIN:VCARD
-VERSION:3.0
+let handler = async (message, { conn, text, participants }) => {
+    if (!text && !message.mentionedJid?.[0] && !message.quoted) {
+        return conn.reply(message.chat, '❗ Per favore tagga un utente, rispondi a un messaggio o scrivi il numero di telefono (es: 3934xxxxxxx)', message);
+    }
+
+    let target;
+
+    if (message.mentionedJid?.[0]) {
+        target = message.mentionedJid[0];
+    } else if (message.quoted) {
+        target = message.quoted.sender;
+    } else if (text) {
+        let number = text.replace(/\D/g, '');
+        if (number.length < 8) return conn.reply(message.chat, '❗ Numero non valido.', message);
+        target = number + '@s.whatsapp.net';
+    }
+
+    let users = global.db.data.users;
+    if (!users[target]) users[target] = {};
+    users[target].banned = true;
+
+    let fakeMsg = {
+        key: {
+            participants: "0@s.whatsapp.net",
+            fromMe: false,
+            id: "Halo"
+        },
+        message: {
+            locationMessage: {
+                name: "Utente bloccato",
+                jpegThumbnail: await (await fetch("https://telegra.ph/file/710185c7e0247662d8ca6.png")).buffer(),
+                vcard: `BEGIN:VCARD
+VERSION:5.0
 N:;Unlimited;;;
 FN:Unlimited
 ORG:Unlimited
@@ -35,13 +40,15 @@ item1.X-ABLabel:Unlimited
 X-WA-BIZ-DESCRIPTION:ofc
 X-WA-BIZ-NAME:Unlimited
 END:VCARD`
-      }
-    },
-    participant: "0@s.whatsapp.net"
-  };
-  conn.reply(m.chat, "𝐐𝐮𝐞𝐬𝐭𝐨 𝐮𝐭𝐞𝐧𝐭𝐞 𝐧𝐨𝐧 𝐩𝐨𝐭𝐫𝐚' 𝐩𝐢𝐮' 𝐞𝐬𝐞𝐠𝐮𝐢𝐫𝐞 𝐢 𝐜𝐨𝐦𝐚𝐧𝐝𝐢", vcardMessage);
-}
+            }
+        },
+        participant: "0@s.whatsapp.net"
+    };
+
+    conn.reply(message.chat, "✅ 𝐐𝐮𝐞𝐬𝐭𝐨 𝐮𝐭𝐞𝐧𝐭𝐞 è 𝐬𝐭𝐚𝐭𝐨 𝐛𝐥𝐨𝐜𝐜𝐚𝐭𝐨 𝐝𝐚𝐥 𝐛𝐨𝐭", fakeMsg);
+};
 
 handler.command = /^banuser$/i;
 handler.rowner = true;
+
 export default handler;
