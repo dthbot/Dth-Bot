c7ec49791186f1814150e110d3e610f27e64dad4
dthbot <blebledarius@gmail.com>
2025-12-27 04:33:22 +0100

    Update fun-culo.js

--- plugins/fun-culo.js
+++ plugins/fun-culo.js
@@ -21,7 +21,7 @@ const inoltra = (nomeDelBot) => {
             forwardingScore: 999,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363259442839354@newsletter',
+                newsletterJid: '',
                 serverMessageId: '',
                 newsletterName: `${nomeDelBot}`
             }
