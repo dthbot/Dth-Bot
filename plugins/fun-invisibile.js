374ecb5de969f1c8f8b6453919a4e583854ede45
dthbot <blebledarius@gmail.com>
2025-12-27 04:35:33 +0100

    Update fun-invisibile.js

--- plugins/fun-invisibile.js
+++ plugins/fun-invisibile.js
@@ -1,15 +1,27 @@
-export const invisible = {
-    name: 'invisible',
-    alias: ['ghost'],
-    desc: 'Rende un utente invisibile nel gruppo 👻',
-    type: 'fun',
+let handler = async (m, { conn }) => {
+    if (!m.isGroup) return conn.sendMessage(m.chat, { text: '❌ Comando solo per gruppi' }, { quoted: m });
+    if (!m.mentionedJid || m.mentionedJid.length === 0) return conn.sendMessage(m.chat, { text: '⚠️ Devi menzionare qualcuno!\nEsempio: .invisible @utente' }, { quoted: m });
 
-    async run({ m, conn }) {
-        if (!m.isGroup) return conn.sendMessage(m.chat, { text: '❌ Comando solo per gruppi' })
-        if (!m.mentionedJid || m.mentionedJid.length === 0)
-            return conn.sendMessage(m.chat, { text: '⚠️ Devi menzionare qualcuno!\nEsempio: .invisible @utente' })
+    let userTag = m.mentionedJid[0].split('@')[0];
+    let messaggio = `👻 Shh! ${userTag} è diventato invisibile… Non puoi vederlo!`;
 
-        const userTag = m.mentionedJid[0].split('@')[0]
-        await conn.sendMessage(m.chat, { text: `👻 Shh! ${userTag} è diventato invisibile… Non puoi vederlo!` })
-    }
-}
+    let opzioniInoltro = inoltra("ChatUnity");
+    await conn.sendMessage(m.chat, { text: messaggio, ...opzioniInoltro }, { quoted: m });
+};
+
+const inoltra = (nomeDelBot) => {
+    return {
+        contextInfo: {
+            forwardingScore: 999,
+            isForwarded: true,
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: '',
+                serverMessageId: '',
+                newsletterName: `${nomeDelBot}`
+            }
+        }
+    };
+};
+
+handler.command = ["invisible", "ghost"];
+export default handler;
