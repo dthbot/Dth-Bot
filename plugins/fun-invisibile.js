21034e1334be5832fd15924a6487b63e1063ca5a
dthbot <blebledarius@gmail.com>
2025-12-27 04:37:20 +0100

    Update fun-invisibile.js

--- plugins/fun-invisibile.js
+++ plugins/fun-invisibile.js
@@ -1,12 +1,14 @@
 let handler = async (m, { conn }) => {
     if (!m.isGroup) return conn.sendMessage(m.chat, { text: '❌ Comando solo per gruppi' }, { quoted: m });
-    if (!m.mentionedJid || m.mentionedJid.length === 0) return conn.sendMessage(m.chat, { text: '⚠️ Devi menzionare qualcuno!\nEsempio: .invisible @utente' }, { quoted: m });
+    if (!m.mentionedJid || m.mentionedJid.length === 0) 
+        return conn.sendMessage(m.chat, { text: '⚠️ Devi menzionare qualcuno!\nEsempio: .invisible @utente' }, { quoted: m });
 
-    let userTag = m.mentionedJid[0].split('@')[0];
-    let messaggio = `👻 Shh! ${userTag} è diventato invisibile… Non puoi vederlo!`;
+    const userJid = m.mentionedJid[0];
+    const userTag = '@' + userJid.split('@')[0]; // tag dell'utente con @
+    const messaggio = `👻 Shh! ${userTag} è diventato invisibile… Non puoi vederlo!`;
 
     let opzioniInoltro = inoltra("ChatUnity");
-    await conn.sendMessage(m.chat, { text: messaggio, ...opzioniInoltro }, { quoted: m });
+    await conn.sendMessage(m.chat, { text: messaggio, mentions: [userJid], ...opzioniInoltro }, { quoted: m });
 };
 
 const inoltra = (nomeDelBot) => {
