f8ccfe214110cfab7184c57d16e88d01193ad0dc
dthbot <blebledarius@gmail.com>
2025-12-28 10:47:40 +0100

    Update gp-scopa.js

--- plugins/gp-scopa.js
+++ plugins/gp-scopa.js
@@ -1,28 +1,34 @@
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn }) => {
 
-    let user = m.mentionedJid?.[0] || m.quoted?.sender;
+  let user = m.mentionedJid?.[0] || m.quoted?.sender;
+  if (!user) throw '❗ Tagga un utente o rispondi a un suo messaggio.';
 
-    if (!user) throw '❗ Tagga un utente o rispondi a un suo messaggio per usare questo comando.';
+  let target = user.split('@')[0];
+  let sender = m.sender.split('@')[0];
 
-    let target = user.split('@')[0];
-    let sender = m.sender.split('@')[0];
+  let message = `
+╭━━━━━━━🔥━━━━━━━╮
+   ✦ 𝐂𝐎𝐍𝐍𝐄𝐒𝐒𝐈𝐎𝐍𝐄 ✦
+╰━━━━━━━🔥━━━━━━━╯
 
+👑 @${sender}
+⚔️ @${target}
 
-    let message = `*🔥 @${sender} sta scopando con @${target}... 💋*`;
+💥 𝐒𝐭𝐚𝐧𝐧𝐨 𝐬𝐜𝐨𝐩𝐚𝐧𝐝𝐨 𝐡𝐚𝐫𝐝 𝐬𝐨𝐭𝐭𝐨 𝐥𝐞 𝐜𝐨𝐩𝐞𝐫𝐭𝐞
+`;
 
+  await conn.reply(m.chat, message, m, {
+    mentions: [user, m.sender]
+  });
 
-    await conn.reply(m.chat, message, m, { mentions: [user, m.sender] });
-
-
-    await conn.sendMessage(m.chat, {
-        react: {
-            text: '💦',
-            key: m.key
-        }
-    });
+  await conn.sendMessage(m.chat, {
+    react: {
+      text: '🔥',
+      key: m.key
+    }
+  });
 };
 
-
 handler.customPrefix = /^\.scopa$/i;
-handler.command = new RegExp; 
+handler.command = new RegExp;
 export default handler;
