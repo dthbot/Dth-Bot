c85a730d87e93d4850091f806378c8cbec4c5fea
dthbot <blebledarius@gmail.com>
2026-01-10 20:18:00 +0100

    Update fun-fp.js

--- plugins/fun-fp.js
+++ plugins/fun-fp.js
@@ -1,29 +1,47 @@
 let handler = async (m, { conn, args }) => {
-    if (!args[0]) throw '❌ Inserisci il numero! Esempio: .fp 393491234567'
+  if (!args[0]) {
+    return m.reply('❌ Usa il comando così:\n*.fp 393xxxxxxxxx*');
+  }
 
-    // Pulisce il numero: rimuove +, -, spazi e caratteri non numerici
-    let number = args[0].replace(/[^0-9]/g, '')
+  // Unisce gli argomenti e rimuove tutto ciò che non è numero
+  let number = args.join('').replace(/\D/g, '');
 
-    // Aggiunge il suffisso WhatsApp se manca
-    if (!number.endsWith('@s.whatsapp.net')) number = number + '@s.whatsapp.net'
+  // Controllo minimo di validità
+  if (number.length < 8) {
+    return m.reply('❌ Numero non valido');
+  }
 
-    try {
-        const url = await conn.profilePictureUrl(number, 'image')
-        await conn.sendMessage(m.chat, { 
-            image: { url }, 
-            caption: `🖼 Foto profilo di *${args[0]}*`
-        }, { quoted: m })
-    } catch (e) {
-        // URL di default valido
-        const defaultImg = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
-        await conn.sendMessage(m.chat, { 
-            image: { url: defaultImg }, 
-            caption: `❌ Non è stato possibile recuperare la foto profilo di *${args[0]}*.\nMostro immagine di default.` 
-        }, { quoted: m })
-    }
-}
+  let jid = number + '@s.whatsapp.net';
 
-handler.help = ['fp']
-handler.tags = ['tools']
-handler.command = ['fp']
-export default handler
+  try {
+    const url = await conn.profilePictureUrl(jid, 'image');
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url },
+        caption: `🖼 Foto profilo di *@${number}*`,
+        mentions: [jid]
+      },
+      { quoted: m }
+    );
+  } catch (e) {
+    const defaultImg =
+      'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
+
+    await conn.sendMessage(
+      m.chat,
+      {
+        image: { url: defaultImg },
+        caption: `❌ Non è stato possibile recuperare la foto profilo di *@${number}*.\nMostro immagine di default.`,
+        mentions: [jid]
+      },
+      { quoted: m }
+    );
+  }
+};
+
+handler.help = ['fp <numero>'];
+handler.tags = ['tools'];
+handler.command = /^fp$/i;
+
+export default handler;
