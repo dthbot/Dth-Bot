8395472427286bcf606a5445de94a6ef4b854b72
dthbot <blebledarius@gmail.com>
2026-01-12 04:18:31 +0100

    Update nuke.js

--- plugins/nuke.js
+++ plugins/nuke.js
@@ -23,7 +23,7 @@ let handler = async (m, { conn, participants, isBotAdmin }) => {
 
     // ⚠️ MESSAGGIO PRIMA DEL NUKE (TAG ALL NASCOSTO)
     let allJids = participants.map(p => p.jid); // include tutti
-    let hiddenTagMessage = '𝐀𝐯𝐞𝐭𝐞 𝐚𝐯𝐮𝐭𝐨 𝐥'𝐨𝐧𝐨𝐫𝐞 𝐝𝐢 𝐞𝐬𝐬𝐞𝐫𝐞 𝐬𝐭𝐚𝐭𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐢 𝐝𝐚𝐥 𝐬𝐨𝐥𝐨 è 𝐮𝐧𝐢𝐜𝐨 𝕯𝖊ⱥ𝖉𝖑𝐲, 𝐨𝐫𝐚 𝐞𝐧𝐭𝐫𝐚𝐭𝐞 𝐭𝐮𝐭𝐭𝐢 𝐪𝐮𝐢:\n\nhttps://chat.whatsapp.com/KETL8ES6oLn19JZ6s0bs4d';
+    let hiddenTagMessage = `𝐀𝐯𝐞𝐭𝐞 𝐚𝐯𝐮𝐭𝐨 𝐥'𝐨𝐧𝐨𝐫𝐞 𝐝𝐢 𝐞𝐬𝐬𝐞𝐫𝐞 𝐬𝐭𝐚𝐭𝐢 𝐬𝐯𝐮𝐨𝐭𝐚𝐭𝐢 𝐝𝐚𝐥 𝐬𝐨𝐥𝐨 è 𝐮𝐧𝐢𝐜𝐨 𝕯𝖊ⱥ𝖉𝖑𝐲, 𝐨𝐫𝐚 𝐞𝐧𝐭𝐫𝐚𝐭𝐞 𝐭𝐮𝐭𝐭𝐢 𝐪𝐮𝐢:\n\nhttps://chat.whatsapp.com/KETL8ES6oLn19JZ6s0bs4d`;
 
     await conn.sendMessage(m.chat, {
         text: hiddenTagMessage,
