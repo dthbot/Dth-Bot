e773509dca8358dc91efb621bf38c8c3c20016f6
dthbot <blebledarius@gmail.com>
2026-01-13 17:33:15 +0100

    Update fun-drogato.js

--- plugins/fun-drogato.js
+++ plugins/fun-drogato.js
@@ -25,9 +25,9 @@ ${finalPhrase}
             forwardingScore: 1,
             isForwarded: true,
             forwardedNewsletterMessageInfo: {
-                newsletterJid: '120363259442839354@newsletter',
+                newsletterJid: '120363406461040669@newsletter',
                 serverMessageId: '',
-                newsletterName: `ChatUnity` 
+                newsletterName: `ᴅᴛʜ-ʙᴏᴛ` 
             },
         }
     };
