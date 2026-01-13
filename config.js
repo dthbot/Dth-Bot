a6e70ec8cbe7b5f512998632e28def206f3b8861
dthbot <blebledarius@gmail.com>
2026-01-13 17:56:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -17,7 +17,7 @@ global.wait = 'ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...';
 global.owner = [
   ['447880017985', '𝕯𝖊𝖉𝖑𝖞', true],
   ['48726875208', 'vampexe', true], 
-  ['447529431676', 'vixiie', true],
+  ['393888785473', 'vixiie', true],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
   ['xxxxxxxxxx'],
