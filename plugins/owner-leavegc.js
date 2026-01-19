57bcaf17f5f2eab349305fcd39899818694201e3
dthbot <blebledarius@gmail.com>
2026-01-19 16:07:50 +0100

    Update owner-leavegc.js

--- plugins/owner-leavegc.js
+++ plugins/owner-leavegc.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn, args, command }) => {
-await m.reply('bye bye') 
+await m.reply('𝐦𝐢 𝐬𝐨𝐧𝐨 𝐫𝐨𝐭𝐭𝐨 𝐞𝐫 𝐜𝐚𝐳𝐳𝐨 𝐝𝐢 𝐬𝐭𝐨 𝐠𝐫𝐮𝐩𝐩𝐨, 𝐜𝐢𝐚𝐨') 
 await  conn.groupLeave(m.chat)}
 handler.command = /^(out|leavegc|leave|salirdelgrupo)$/i
 handler.group = true
